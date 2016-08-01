0c15c646b506f3ef02aab4f2c7b30700e20d4f11
cassac <hicassac@gmail.com>
2016-07-31 23:02:06 -0700

    finishes add and remove methods

--- sprint-two/src/linkedList.js
+++ sprint-two/src/linkedList.js
@@ -4,12 +4,31 @@ var LinkedList = function() {
   list.tail = null;
 
   list.addToTail = function(value) {
+    var newNode = Node(value);
+    if (!list.head) {
+      list.head = newNode;
+    }
+    list.tail = newNode;
   };
 
   list.removeHead = function() {
+    var oldHead = list.head;
+    list.head = list.tail;
+    list.tail = list.tail.next;
+    return oldHead.value;
   };
 
   list.contains = function(target) {
+    var node = list.head;
+    while (node) {
+      if (node.value === target) {
+        console.log('contains:', target)
+        return true;
+      }
+      node = node.next;
+    }
+    console.log('to false')
+    return false;
   };
 
   return list;
