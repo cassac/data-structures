f2fca2af9675cd5678e310eee613d8d3581708f9
cassac <hicassac@gmail.com>
2016-08-02 22:33:20 -0700

    adds insert method

--- sprint-two/src/binarySearchTree.js
+++ sprint-two/src/binarySearchTree.js
@@ -1,6 +1,42 @@
 var BinarySearchTree = function(value) {
+  var binaryTree = Object.create(binaryMethods);
+  binaryTree.value = value;
+  binaryTree.left = null;
+  binaryTree.right = null;
+  return binaryTree;
 };
 
+binaryMethods = {};
+
+binaryMethods.insert = function(value) {
+  var newBinaryTree = BinarySearchTree(value);
+  var node = this;
+  while (node.value) {
+    if (value > node.value) {
+      if (node.right) {
+        node = node.right;
+      } else {
+        node.right = newBinaryTree;
+        return;
+      }
+    } else {
+      if (node.left) {
+        node = node.left;
+      } else {
+        node.left = newBinaryTree;
+        return;
+      }
+    }
+  }
+};
+
+binaryMethods.contains = function(value) {
+
+};
+
+binaryMethods.depthFirstLog = function() {
+
+};
 
 /*
  * Complexity: What is the time complexity of the above functions?
